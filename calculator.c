f322bafa44db1a46cd470915da3c6fcd529da80d
hello-osy <150113262+hello-osy@users.noreply.github.com>
2023-11-23 21:18:38 +0900

    11/23 코드
    -원래 숫자를 묶어서 노드 하나에 저장하려고 했잖아요? 하다보니까 뭔가 안 되더라고요.  띄어쓰기로 수를 구분해보는 코드로 바꿔봤어요.
    -식을 입력받을 때, 공백, 오타 무시함.
    -소숫점은 그대로 가져옴.

--- calculator.c
+++ calculator.c
@@ -19,10 +19,10 @@ LINK string_to_list(char s[]){
 //연결리스트 관련 코드
 struct NODE{
     struct NODE *next;
-    int data;
+    char data;
 };
 
-void addNext(struct NODE *target, int data){
+void addNext(struct NODE *target, char data){
     struct NODE *newNode = malloc(sizeof(struct NODE));
     newNode->next = target->next;
     newNode->data = data;
@@ -45,67 +45,94 @@ LINK infix_to_postfix(LINK infix){
     struct NODE *postfix_head = malloc(sizeof(struct NODE));
     postfix_head->next=NULL;
 
-    struct NODE *num_append_head = malloc(sizeof(struct NODE));
-    num_append_head->next=NULL;
-
     int signal=0;
-    while (infix->d != "\0"){
+    while (infix->next != NULL){
         if (infix->d == "("){
-            addNext(temp_head, removeNext(infix) - 48); //removeNext리턴값은 char, addNext의 두 번쨰 인자는 int
+            addNext(postfix_head, " ");
+            addNext(temp_head, removeNext(infix));
             signal=0;
         }
         else if (infix->d == ")"){
+            addNext(postfix_head, " ");
             while (temp_head->data != "("){
-                addNext(postfix_head, removeNext(temp_head) - 48);
+                addNext(postfix_head, removeNext(temp_head));
             }
             signal=0;
         }
         else if((infix->d == "*") || (infix->d == "/")){
+            addNext(postfix_head, " ");
             if (signal<2){
-                addNext(temp_head, removeNext(infix) - 48);
+                addNext(temp_head, removeNext(infix));
             }
             else{
-                addNext(postfix_head, removeNext(temp_head) - 48);
-                addNext(temp_head, removeNext(infix) - 48);
+                addNext(postfix_head, removeNext(temp_head));
+                addNext(temp_head, removeNext(infix));
             }
             signal=2;
         }
         else if((infix->d == "+") || (infix->d == "-")){
+            addNext(postfix_head, " ");
             if (signal<1){
-                addNext(temp_head, removeNext(infix)- 48);
+                addNext(temp_head, removeNext(infix));
             }
             else{
-                addNext(postfix_head, removeNext(temp_head)- 48);
-                addNext(temp_head, removeNext(infix)- 48);
+                addNext(postfix_head, removeNext(temp_head));
+                addNext(temp_head, removeNext(infix));
             }
             signal=1;
         }
-        else{
-            while ((infix->d == "0") || (infix->d == "1") || (infix->d == "2") || (infix->d == "3") || (infix->d == "4") || (infix->d == "5") || (infix->d == "6") || (infix->d == "7") || (infix->d == "8") || (infix->d == "9")){
-                addNext(num_append_head, removeNext(infix)- 48);
-            }
-            int number=0;
-            int product=1;
-            while (num_append_head->next != NULL){
-                int num_fragment=removeNext(num_append_head)-48; //char->int 변환
-                number=number+ num_fragment*product;
-                product*=10;   
-            }
-            //숫자를 number로 묶어서 한 덩어리로 만들기는 했다. 그런데 이거를 postfix 스택에 추가할 수 있나? postfix스택은 한 노드가 int타입데이터만 저장 가능. 아 가능하구나.
-            addNext(postfix_head, number);
-        }
+        else if ((infix->d == "0") || (infix->d == "1") || (infix->d == "2") || (infix->d == "3") || (infix->d == "4") || (infix->d == "5") || (infix->d == "6") || (infix->d == "7") || (infix->d == "8") || (infix->d == "9")){
+            addNext(postfix_head, removeNext(infix));
+        } //일단 공백 입력은 처리했음. //오타도 처리했음.
+        else if(infix->d == "."){
+            addNext(postfix_head, removeNext(infix));
+        } //소숫점도 일단 그대로 가져옴.
     }
     return postfix_head;
 }
+//후위표기식에서 숫자들을 띄어쓰기로 구분했다.
 
+//연산 과정
+int calculate_postfix(LINK postfix){
+    struct NODE *temp1_head = malloc(sizeof(struct NODE));
+    temp1_head->next=NULL;
+
+    struct NODE *temp2_head = malloc(sizeof(struct NODE));
+    temp2_head->next=NULL;
+
+    while(postfix->next!=NULL){  //후위표기법 계산은 왼쪽에서부터 하므로, 순서를 반대로 바꿔서 다른 스택에 저장한다.
+        addNext(temp1_head, removeNext(postfix));
+    }
+
+    while(temp1_head->next!=NULL){
+        if ((temp1_head->data == "0") || (temp1_head->data == "1") || (temp1_head->data == "2") || (temp1_head->data == "3") || (temp1_head->data == "4") || (temp1_head->data == "5") || (temp1_head->data == "6") || (temp1_head->data == "7") || (temp1_head->data == "8") || (temp1_head->data == "9")){
+            //여기부터 구현 이어가기.
+        }
+        
+    }
+}
 
 //덧셈
+int add_postfix(int a, int b){
+    //내용 구현하기
+    return 0;
+}
 
 //뺄셈
-
+int subtract_postfix(int a, int b){
+    //내용 구현하기
+    return 0;
+}
 //곱셈
-
+int multiply_postfix(int a, int b){
+    //내용 구현하기
+    return 0;
+}
 //나눗셈
+int divide_postfix(int a, int b){
+    //내용 구현하기
+    return 0;
+}
 
 int main(void){
     FILE *ifp, *ofp;
