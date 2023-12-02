7a11663638840e749eacf59e6074f39605bf200c
hello-osy <150113262+hello-osy@users.noreply.github.com>
2023-12-02 11:02:57 +0900

    12/02
    -코드 오류 수정
    -add.c 파일의 Add를 호출했음.

--- calculator.c
+++ calculator.c
@@ -1,5 +1,6 @@
 #include "calculator.h"
 #include "list.h"
+#include "add.h"
 //LINK 관한 설명은 list.h참고
 //LINK는 그냥 연결리스트임.
 
@@ -22,7 +23,7 @@ struct NODE{
     char data;
 };
 
-void addNext(struct NODE *target, char data){
+void addNext(struct NODE *target, char data){//target 다음에 새로운 노드를 추가해줌.
     struct NODE *newNode = malloc(sizeof(struct NODE));
     newNode->next = target->next;
     newNode->data = data;
@@ -30,10 +31,11 @@ void addNext(struct NODE *target, char data){
     target->next = newNode;
 }
 
-char removeNext(struct NODE *target){
+char removeNext(struct NODE *target){ //target노드를 지워줌.
     struct NODE *removeNode = target->next;
     target->next = removeNode->next;
     char data = target->data;
+    target->data=removeNode->data;
     free(removeNode);
     return data;
 }
@@ -46,14 +48,13 @@ LINK infix_to_postfix(LINK infix){
     postfix_head->next=NULL;
 
     int signal=0;
-    while (infix->next != NULL){
+    while (infix->next != NULL){ //수정해야함. 마지막 문자가 while문에서 처리되지 않음.
         if (infix->d == "("){
-            addNext(postfix_head, " ");
+            addNext(postfix_head, " "); //숫자가 아닌 애들은 띄어쓰기로 구분하기 위해서 넣은 코드임.
             addNext(temp_head, removeNext(infix));
             signal=0;
         }
         else if (infix->d == ")"){
-            addNext(postfix_head, " ");
             while (temp_head->data != "("){
                 addNext(postfix_head, removeNext(temp_head));
             }
@@ -100,44 +101,24 @@ int calculate_postfix(LINK postfix){
     struct NODE *temp2_head = malloc(sizeof(struct NODE));
     temp2_head->next=NULL;
 
-    while(postfix->next!=NULL){  //후위표기법 계산은 왼쪽에서부터 하므로, 순서를 반대로 바꿔서 다른 스택에 저장한다.
-        addNext(temp1_head, removeNext(postfix));
-    }
-
-    while(temp1_head->next!=NULL){
-        if ((temp1_head->data == "0") || (temp1_head->data == "1") || (temp1_head->data == "2") || (temp1_head->data == "3") || (temp1_head->data == "4") || (temp1_head->data == "5") || (temp1_head->data == "6") || (temp1_head->data == "7") || (temp1_head->data == "8") || (temp1_head->data == "9")){
-            //여기부터 구현 이어가기.
+    while(postfix->next!=NULL){//수정해야함. 마지막 문자를 처리할 수 없음.
+        int signal=1;
+        if ((postfix->d == ".") ||(postfix->d == "0") || (postfix->d == "1") || (postfix->d == "2") || (postfix->d == "3") || (postfix->d == "4") || (postfix->d == "5") || (postfix->d == "6") || (postfix->d == "7") || (postfix->d == "8") || (postfix->d == "9")){
+            if (signal==1){
+                while(postfix->d != " "){ //공백 나오기 전까지. 그러니까 하나의 숫자 덩어리를 처리하는 부분임.
+                    addNext(temp1_head,removeNext(postfix)); //첫 번째 숫자 덩어리
+                    signal=2;
+                }
+            }
+            else if (signal==2){
+                while(postfix->d != " "){ //공백 나오기 전까지. 그러니까 하나의 숫자 덩어리를 처리하는 부분임.
+                    addNext(temp2_head,removeNext(postfix)); //두 번째 숫자 덩어리
+                    signal=1;
+                }
+            }
         }
-        
+        else if((postfix->d == "+")){
+            Add(temp1_head,temp2_head);
+        }
     }
 }
-
-//덧셈
-int add_postfix(int a, int b){
-    //내용 구현하기
-    return 0;
-}
-
-//뺄셈
-int subtract_postfix(int a, int b){
-    //내용 구현하기
-    return 0;
-}
-//곱셈
-int multiply_postfix(int a, int b){
-    //내용 구현하기
-    return 0;
-}
-//나눗셈
-int divide_postfix(int a, int b){
-    //내용 구현하기
-    return 0;
-}
-
-
-
-//소수를 정수로 바꿔주는 함수
-
-//계산 결과를 소수로 보정해주는 함수
-
-//식은 문자열로 오는데, 그 안에 잘못 입력된 애도 있고, 공백 문자도 있고, 연산자, 피연산자가 있다. 예외처리할지, 제거할 건 제거할지 결정하고 프로그래밍하자.
